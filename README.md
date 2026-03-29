e36d6c90b5ad15f1245472f5db3320e93cc2d555
Joaquín Llallire <joaquin.llallire@utec.edu.pe>
2026-03-29 03:47:30 -0500

    Nuevo par de claves

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Primer cambio: creamos una linea
+Un cambio mas: ya obtuvimos nuestras claves SSH
--- main.cpp
+++ main.cpp
@@ -6,4 +6,5 @@ int main()
     cout<<"Hola";
     cout<<"Esto es una prueba para mi primer commit";
     cout<<"Agregamos esto para el tercer commit";
+    cout<<"Ahora tengo mis claves SSH creadas";
 }
